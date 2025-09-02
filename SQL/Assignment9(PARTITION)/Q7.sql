a1937c15ee5f037e873b0cbcf3f04df27fba176e
Pranav-ONLY <shendkarpranav0@gmail.com>
2025-09-02 20:50:39 +0530

    Added assignment 9 last 7th file

--- SQL/Assignment9(PARTITION)/Q7.sql
+++ SQL/Assignment9(PARTITION)/Q7.sql
@@ -2,14 +2,20 @@
 for deptno 20. query should display
 ename, sal, diff between max and employees's sal
 diff between min sal and max sal of the dept.
-
++-------+---------+---------+---------+---------------------+---------------------+
+| ename | sal     | maximum | minimum | difference with sal | difference with min |
++-------+---------+---------+---------+---------------------+---------------------+
+| SMITH |  800.00 | 3000.00 |  800.00 |             2200.00 |             2200.00 |
+| JONES | 2975.00 | 3000.00 |  800.00 |               25.00 |             2200.00 |
+| SCOTT | 3000.00 | 3000.00 |  800.00 |                0.00 |             2200.00 |
+| ADAMS | 1100.00 | 3000.00 |  800.00 |             1900.00 |             2200.00 |
+| FORD  | 3000.00 | 3000.00 |  800.00 |                0.00 |             2200.00 |
++-------+---------+---------+---------+---------------------+---------------------+
 */
-SELECT ename, sal,
-MAX(sal)OVER (PARTITION BY deptno) AS max,
-MIN(sal)OVER(PARTITION BY deptno) AS min,
-MAX(sal)-sal OVER(PARTITION BY deptno) AS "emp diff with max"
-FROM emp
+SELECT ename, sal, m.maximum, m.minimum,
+(m.maximum-sal) AS "difference with sal",
+(m.maximum-m.minimum) AS "difference with min"
+FROM emp,
+(SELECT MAX(sal) AS maximum, MIN(sal) AS minimum FROM emp WHERE deptno=20) AS m
 WHERE deptno = 20;
 
-MAX(sal)-sal AS "emp diff with max"
-MAX(sal)-MIN(sal) OVER(PARTITION BY deptno) AS difference,
